a9e3e09423d863292ae5dbdde4b39b4770cee581
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2016-08-30 00:02:51 +0200

    Update profile page to display the users microposts

--- public/app/scripts/controllers/user.js
+++ public/app/scripts/controllers/user.js
@@ -4,8 +4,8 @@
  * Controller for showing a user's profile page
  */
 angular.module('microblogApp')
-  .controller('UserCtrl', ['$scope', '$http', '$routeParams', 'ConfigService',
-    function ($scope, $http, $routeParams, ConfigService) {
+  .controller('UserCtrl', ['$scope', '$http', '$routeParams', 'ConfigService', 'CurrentUserService',
+    function ($scope, $http, $routeParams, ConfigService, CurrentUserService) {
 
       var getUserData = function() {
         $http({
@@ -26,7 +26,29 @@ angular.module('microblogApp')
         });
       };
 
+      var getBlogPosts = function() {
+        console.log('auth token: ',ConfigService.getToken());
+        console.log('user id: ', CurrentUserService.getId());
+        $http({
+          method: 'GET',
+          url: ConfigService.apiUrl() + 'microposts/' + $routeParams.id,
+          headers: {
+            'Authorization': ConfigService.getToken()
+          }
+        })
+        .success(function(data) {
+          console.log('success');
+          console.log(data);
+          $scope.microposts = data.microposts.slice().reverse();
+        })
+        .error(function(data) {
+          console.log('error');
+          console.log(data);
+        });
+      };
+
       getUserData();
+      getBlogPosts();
 
     }
     
